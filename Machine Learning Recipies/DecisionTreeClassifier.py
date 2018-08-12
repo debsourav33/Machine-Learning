c8320ccff45fbe622912e6dd72163c3d2ed1c16d
debsourav33 <debsourav33@gmail.com>
2018-08-13 00:45:09 +0600

    Added support for unseen data (DecisionTree)

--- Machine Learning Recipies/DecisionTreeClassifier.py
+++ Machine Learning Recipies/DecisionTreeClassifier.py
@@ -1,117 +1,111 @@
-training_data=[ ['Green',3,'Apple'],
-                ['Yellow',3,'Apple'],
-                ['Red',1,'Grape'],
-                ['Red',1,'Grape'],
-                ['Yellow',3,'Lemon']
-               ]
+header= ['color','size','label']
 
-def unique_vals(data,col):
-    return set(row[col] for row in data)
 
-def class_count(data):
-
-    count= {}
+def class_counts(data):
+    counts = {}
 
     for row in data:
-        label= row[-1]
+        lbl = row[-1]
+        if lbl not in counts:
+            counts[lbl] = 0
+        counts[lbl] += 1
 
-        if label not in count:
-            count[label]=0
-
-        count[label]+=1
-
-    return count
-
-def isNumeric(value):
-    return isinstance(value,int) or isinstance(value,float)
-
-class Question():
-    def __init__(self,col,value):
-        self.col=col
-        self.value=value
-
-    def match(self,example):
-        if(isNumeric(self.value)):
-            return example[self.col]>=self.value
-        return example[self.col] == self.value
+    return counts
 
 
-def partition(data,question):
-    true_rows, false_rows= [], []
+def is_numeric(value):
+    return isinstance(value, int) or isinstance(value, float)
+
+
+class Question:
+    def __init__(self, col, value):
+        self.col = col
+        self.value = value
+
+    def match(self,x):
+        if(is_numeric(self.value)):
+            return x[self.col]>=self.value
+        else:
+            return x[self.col]==self.value
+
+    def __repr__(self):
+        if(is_numeric(self.value)):
+            condition=">="
+        else:
+            condition="=="
+
+        return "Is %s %s %s?" % (header[self.col],condition,self.value)
+
+
+def partition(data, question):
+    true_rows, false_rows= [],[]
 
     for row in data:
-        if question.match(row):
+        if(question.match(row)):
             true_rows.append(row)
         else:
             false_rows.append(row)
+
 
     return true_rows, false_rows
 
 
 def gini(data):
 
-    impurity=1
+    impurity= 1
+    counts= class_counts(data)
+    length= len(data)
 
-    count= class_count(data)
-    for label in count:
-        prb= count[label]/(float)(len(data))
+    for lbl in counts:
+        prb= counts[lbl]/length
         impurity-=prb**2
 
     return impurity
 
-def infoGain(current_uncertainity, true_rows, false_rows):
-    p= (len(true_rows)/(float)(len(true_rows)+len(false_rows)))
+def info_gain(data,true_rows,false_rows):
 
-    return current_uncertainity- p*gini(true_rows)- (1-p)*gini(false_rows)
+    p= len(true_rows)/float(len(data))
+    return gini(data)- p*gini(true_rows)- (1-p)*gini(false_rows)
 
 
-def findBestSplit(data):
+def find_best_split(data):
+    best_question= None
+    best_gain= 0
 
-    best_gain=0
-    best_Question= None
+    n_features= len(training_data[0])-1
 
-    current_uncertainity= gini(data)
-
-    length= len(data[0])
-
-    for col in range(length):
-
-        values= unique_vals(data,col)
+    for col in range(n_features):
+        values= set(x[col] for x in data)
 
         for val in values:
-
             q= Question(col,val)
 
             true_rows, false_rows= partition(data,q)
 
-            if((len(true_rows)==0)or(len(false_rows)==0)):
+            if len(true_rows)==0 or len(false_rows)==0:
                 continue
 
-            current_gain= infoGain(current_uncertainity,true_rows,false_rows)
+            gain= info_gain(data,true_rows,false_rows)
+            if gain>best_gain:
+                best_gain=gain
+                best_question=q
 
-            if(current_gain>=best_gain):
-                best_gain=current_gain
-                best_Question=q
+    return best_gain, best_question
 
-
-    return best_gain,best_Question
-
-class Leaf():
-
+class Leaf:
     def __init__(self,data):
-        self.prediction=class_count(data)
+        self.prediction= class_counts(data)
 
-class DecisionNode():
-
-    def __init__(self,question,true_branch,false_brance):
-        self.true_branch=true_branch
-        self.false_branch= false_brance
+class decision_node:
+    def __init__(self,true_branch,false_branch,question):
+        self.true_branch= true_branch
+        self.false_branch= false_branch
         self.question= question
 
 
 def build_tree(data):
 
-    gain, question= findBestSplit(data)
+    gain, question= find_best_split(data)
 
     if(gain==0):
         return Leaf(data)
@@ -121,32 +115,74 @@ def build_tree(data):
     true_branch= build_tree(true_rows)
     false_branch= build_tree(false_rows)
 
-    return DecisionNode(question,true_branch,false_branch)
+    return decision_node(true_branch,false_branch,question)
 
 
-def classify(node, example):
+def print_tree(node,spacing=" "):
+    """The world's most elegant tree printing function"""
 
-    if isinstance(node,Leaf):
-        return node.prediction
+    if(isinstance(node,Leaf)):
+        print(spacing+"Prediction:",node.prediction)
+        return
 
-    if(node.question.match(example)):
-        return classify(node.true_branch,example)
-    else:
-        return classify(node.false_branch, example)
+    print(spacing+str(node.question))
 
-def printLeaf(counts):
+    print(spacing+'-->True')
+    print_tree(node.true_branch,spacing+"  ")
 
-    total= sum(counts.values()) * 1.0
-    probs={}
+    print(spacing+'-->false')
+    print_tree(node.false_branch,spacing+"  ")
+
+def print_accuracy(counts):
+    length=0
+    for lbl in counts:
+        length+=counts[lbl]
 
     for lbl in counts:
-        probs[lbl] = str(int(counts[lbl]/total*100))+"%"
+        print("%s= %.2f%%" %(lbl,counts[lbl]*100/length))
 
-    print(probs)
+def traverse(node,x):
+    if isinstance(node,Leaf):
+        print_accuracy(node.prediction)
+        return
+
+    if(node.question.match(x)):
+        traverse(node.true_branch,x)
+    else:
+        traverse(node.false_branch,x)
+
+def predict(root,data):
+    prediction=[]
+    cnt=1
+
+    for x in data:
+        print("\nPrediction accuracy for data #%d:"%(cnt))
+        cnt+=1
+        traverse(root,x)
+
+    return prediction
 
 if __name__ == '__main__':
-    my_tree= build_tree(training_data)
-    printLeaf(classify(my_tree,training_data[1]))
+    training_data = [['Green', 3, 'Apple'],
+                     ['Yellow', 3, 'Apple'],
+                     ['Red', 1, 'Grape'],
+                     ['Red', 1, 'Grape'],
+                     ['Yellow', 3, 'Lemon']
+                     # ['Green', 4, 'Apple'],
+                     # ['Red', 4, 'Mango'],
+                     # ['Blue', 1, 'Berry'],
+                     # ['Yellow', 2, 'Lemon']
+                     ]
+
+    root= build_tree(training_data)
+    #print_tree(root)
+    test=[['Yellow',3],['Red',3]]
+    predict(root,test)
+
+
+
+
+
 
 
 
